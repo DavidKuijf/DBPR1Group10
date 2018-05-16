049cb4dca406db9f6709096cfd14612452378f4e
davici <Davici@live.nl>
2018-05-16 13:01:22 +0200

    external css sheet should work now

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+    <title>DBPRGroup10</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <style>
+        
+        body,h1 {
+            font-family: Raleway, sans-serif;
+            color: white;
+            text-shadow:-1px 0 black, 0 1px black, 1px 0 black, 0 -1px black; 
+
+        }
+        body, html {height: 100%}
+    }
+    </style>
+    <head >
+        <link rel="stylesheet" type="text/css" href="default.css">
+        <?php
+            echo "Hello World!";
+        ?> 
+         
+    </head>
+        <div class="bgimg">
+        <body class="bgimg" style="margin: 0">
+        
+       
+
+    </body>
+</html> 
---
+++ testpage.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <title>DBPRGroup10</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <head>
+        <div class="bgimg">
+        
+        
+    </head>
+    <body style="margin: 0">
+        
+       
+
+    </body>
+</html> 
