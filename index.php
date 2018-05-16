9e1be6e0cfb8118c03b23bca0e557811b3b44978
davici <Davici@live.nl>
2018-05-16 14:39:48 +0200

    Fade in center text

--- index.php
+++ index.php
@@ -2,26 +2,22 @@
 <html>
     <title>DBPRGroup10</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <style>
-        
-        
-    }
-    </style>
+    
     <head >
+        <meta name="robots" content="noindex">
         <link rel="stylesheet" type="text/css" href="default.css">
         <link rel"animationsheet" type="text/css" hraf="animation.css"
         
     </head>
         
+   
+    <body class="bgimg">
+        <div class="center">
+        <div class="fade-in"><h1>REEEEEEEEEEEEEEEE</h1></div>
         
-    <body class="bgimg" style="margin: 0">
-        <div class="fade-in"> <h1>REEEEEEEEEEEE</h1></div>   
-        <?php
-            echo "Hello World!";
-        ?> 
-         
-        <a href="/testpage.php">
-       
-
+        </div>
     </body>
+
+    
+    
 </html> 
