4f42e56c5d7289ea12162c7bc458fed576159570
Jordan <SubwoofGaming@gmail.com>
2018-05-19 13:28:25 +0200

    quickmatch-needs-css-still

---
+++ quickmatch.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+    <title>DBPRGroup10</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <head>
+        <meta name="robots" content="noindex">
+        <link rel="stylesheet" type="text/css" href="default.css">
+        <link rel"animationsheet" type="text/css" hraf="animation.css">
+        <script src="quickmatch.js"></script>
+       
+        
+        
+    </head>
+    <body class="bgimg">
+        <div class="center">
+            <button id="addone" onclick="addScoreOne()">+</button>
+            <button id="addtwo" onclick="addScoreTwo()">+</button><br>
+            <text id="scoreteamone">0</text>
+            <text id="scoreteamtwo">0</text><br>
+            <button id="decone" onclick="decScoreOne()">-</button>
+            <button id="dectwo" onclick="decScoreTwo()">-</button>
+        </div>
+    </body>
+</html> 
