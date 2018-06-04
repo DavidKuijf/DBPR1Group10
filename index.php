eb37f62444dcf785549153d9afef1ee877dc108e
davici <Davici@live.nl>
2018-06-04 08:29:44 +0200

    reee

--- index.php
+++ index.php
@@ -15,26 +15,18 @@
 
 
     <script>
-    var variablejs = "<?php echo $variablephp; ?>" ;
-    alert("category = " + variablejs);
-    </script>
-        <div id="loginbit">
-        </div>
-       
-        <script>
-            
-            
-            if(<?php echo $_SESSION ?>=null){
-            var xhttp = new XMLHttpRequest();
-            xhttp.onreadystatechange = function() {
-                if (this.readyState == 4 && this.status == 200) {
-                document.getElementById("loginbit").innerHTML = this.responseText;
-                }
-            };
-            xhttp.open("GET", "loginform.html", true);
-            xhttp.send();
+       $.ajax({
+            url: 'sessioncheck.php',
+            type: 'POST',
+            success: function(result) {
+                if (result == 'success'){
+                alert('woohoo');
+                }else{
+                alert('waahaa');
             }
-        </script>
+        }
+    });
+    </script>
         
         
 
@@ -43,10 +35,10 @@
             <div class="fade-in"> 
                 
                 <div id="menu" class="menu">
-                    <a href="scoreboard.php" class"menuButton"><img src="/Images/vs.png" class='reframe'></a>
-                    <a href="testpage.php" class"menuButton"><img src="/Images/podium.png" class='reframe'></a>
-                    <a href="userlist.php" class"menuButton"><img src="/Images/tournament.png" class='reframe'></a>
-                    <a href="testpage.php" class"menuButton"><img src="/Images/gears.png" class='reframe'></a>
+                    <a href="scoreboard.php" class"menuButton"><img src="Images/vs.png" class='reframe'></a>
+                    <a href="testpage.php" class"menuButton"><img src="Images/podium.png" class='reframe'></a>
+                    <a href="userlist.php" class"menuButton"><img src="Images/tournament.png" class='reframe'></a>
+                    <a href="testpage.php" class"menuButton"><img src="Images/gears.png" class='reframe'></a>
 
                 </div>
             </div>
